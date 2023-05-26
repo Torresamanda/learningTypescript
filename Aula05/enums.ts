36dcfe4edf9f857364659ec413a0b42dd95bd99c
Amanda Torres <amandakaia@hotmail.com>
2023-05-26 13:23:59 -0300

    aula 4&5 e ignorando packages

---
+++ Aula05/enums.ts
@@ -0,0 +1,18 @@
+//Exclusivo do TypeScripit
+//enums é um conjunto de constantes 11
+
+enum Planets {
+    MERCURIO = 'Mercurio',
+    VENUS = 'Venus',
+    TERRA = 'Terra',
+    MARTE = 'Marte'
+}
+
+Planets.TERRA
+
+enum Roles {
+    ADMIN,
+    USER
+}
+
+Roles.ADMIN
